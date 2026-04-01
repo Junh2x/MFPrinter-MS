f7ef65d4b005328f938bbf5f221b51af1ba9da0f
Junhx2 <zzlz2004@naver.com>
2026-04-01 16:57:21 +0900

    test: test4.py => AID 지정 등록 성공, 목록 파싱 실패
    alframe.cgi req의 AID 파라미터 누락 시, 서버측에서 AID를 1로 고정하는 것으로 예상

--- remote-tools/canon_test4.py
+++ remote-tools/canon_test4.py
@@ -47,6 +47,9 @@ def main():
     s = requests.Session()
     s.verify = False
 
+    # fix
+    aid = "11"
+
     common = {
         "User-Agent": UA,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
